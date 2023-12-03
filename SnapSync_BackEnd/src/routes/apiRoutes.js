0cbe4e041d2e40325f73b8049df2b85452903e4d
RAMMACHyc <sadik.ram12@gmail.com>
2023-12-03 23:54:49 +0100

    update route for getPosts

--- SnapSync_BackEnd/src/routes/apiRoutes.js
+++ SnapSync_BackEnd/src/routes/apiRoutes.js
@@ -5,7 +5,6 @@ import { PostController } from "../controllers/postController";
 const router = Router();
 // const { protect } = require('../middleware/authMiddleware').default
 router.post("/", PostController.createPost);
-router.get("/posts", PostController.getPaginatedPosts);
 router.get("/", PostController.getPosts);
 router.patch("/like/:id", PostController.likePost);
 router.patch('/:id', PostController.updatePost);
